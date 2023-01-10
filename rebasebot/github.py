8312daf58e9e3598066092f70d15b68c4a05206f
Denis Moiseev <1239415+lobziik@users.noreply.github.com>
2023-01-10 12:48:38 +0100

    Extract logic around github credentials into separate class
    This commit introduces GithubAppProvider
    which contain logic around `user_auth` parameter
    which previously was spreaded
    across the rebasebot.bot module code.
    
    This class is intended to be instantiated right after
    the argument parsing step and passed into `bot.run`
    function, instead of a set of tokens.
    Now authenticated github3.GitHub instances might be
    obtained through this wrapper.

---
+++ rebasebot/github.py
@@ -0,0 +1,169 @@
+"""Contains GitHub related helper classes."""
+
+import logging
+from dataclasses import dataclass
+
+from functools import cached_property
+from typing import Optional
+
+import github3
+
+logger = logging.getLogger()
+
+
+@dataclass
+class GitHubBranch:
+    """
+    GitHubBranch specifies GitHub repository along with a branch there.
+
+    :url:       full GitHub url
+    :ns:        namespace (example: 'openshift' in 'https://github.com/openshift/api')
+    :name:      repository name (example: 'api' in 'https://github.com/openshift/api')
+    :branch:    branch name, 'main' for example
+    """
+    url: str
+    ns: str  # pylint: disable=invalid-name
+    name: str
+    branch: str
+
+
+@dataclass
+class GitHubAppCredentials:
+    """
+    GitHubAppCredentials holds credentials for GitHub app.
+
+    :github_branch: uses for specifying repository where app is installed.
+    """
+    app_id: int
+    app_key: bytes
+    github_branch: GitHubBranch
+
+
+class GithubAppProvider:
+    """
+    GithubAppProvider helper for constructing and holding authenticated GitHub app.
+
+    Might operate with user API token, or with GithHub app credentials.
+
+    In case user_auth is specified, app-related logic would not be engaged
+    and user credentials will be used.
+
+    For the 'app' mode this provider requires two application credentials sets:
+    * app: application which is installed in target repository (where rebase PR will be created)
+    * cloner: application which is installed into intermediate (rebase) repository
+              (where resulting git tree will be pushed)
+    """
+    _app_credentials: Optional[GitHubAppCredentials]
+
+    _cloner_app_credentials: Optional[GitHubAppCredentials]
+
+    user_auth: bool
+    user_token: Optional[str]
+
+    def __init__(
+            self,
+            app_id: Optional[int] = None,
+            app_key: Optional[bytes] = None,
+            dest_branch: Optional[GitHubBranch] = None,
+
+            cloner_id: Optional[int] = None,
+            cloner_key: Optional[bytes] = None,
+            rebase_branch: Optional[GitHubBranch] = None,
+
+            user_auth: bool = False,
+            user_token: Optional[str] = None,
+    ):
+        self.user_auth = user_auth
+        self.user_token = user_token
+        self._app_credentials = None
+        self._cloner_app_credentials = None
+
+        if not user_auth:
+            if not all(
+                (app_id, app_key, dest_branch, cloner_id, cloner_key, rebase_branch)
+            ):
+                raise ValueError("Credentials for both, cloning and pushing app should be provided")
+
+            self._app_credentials = GitHubAppCredentials(
+                app_id=app_id, app_key=app_key, github_branch=dest_branch
+            )
+
+            self._cloner_app_credentials = GitHubAppCredentials(
+                app_id=cloner_id, app_key=cloner_key, github_branch=rebase_branch
+            )
+
+    def get_app_token(self) -> str:
+        """
+        Get app auth token
+
+        :return: str
+        """
+        return self.github_app.session.auth.token
+
+    def get_cloner_token(self) -> str:
+        """
+        Get cloner app auth token
+
+        :return: str
+        """
+        return self.github_cloner_app.session.auth.token
+
+    @cached_property
+    def github_app(self) -> github3.GitHub:
+        """
+        Authenticated GitHub app.
+
+        In case `user_auth` = True, returns app authenticated with user token.
+        In app mode `app_id`, `app_key` and `dest_branch` will be used for app authentication.
+
+        :return: github3.GitHub
+        """
+        if self.user_auth:
+            return self._get_github_user_logged_in_app()
+
+        return self._github_login_app(self._app_credentials)
+
+    @cached_property
+    def github_cloner_app(self) -> github3.GitHub:
+        """
+        Authenticated GitHub app.
+
+        In case `user_auth` = True, returns app authenticated with user token.
+        In app mode `cloner_id`, `cloner_key` and `rebase_branch`will be used for app authentication.
+
+        :return: github3.GitHub
+        """
+        if self.user_auth:
+            return self._get_github_user_logged_in_app()
+
+        return self._github_login_app(self._cloner_app_credentials)
+
+    @staticmethod
+    def _github_login_app(credentials: GitHubAppCredentials) -> github3.GitHub:
+        logging.info(
+            "Logging to GitHub as an Application for repository %s", credentials.github_branch.url
+        )
+        gh_app = github3.GitHub()
+        gh_app.login_as_app(credentials.app_key, credentials.app_id, expire_in=300)
+        gh_branch = credentials.github_branch
+
+        try:
+            install = gh_app.app_installation_for_repository(
+                owner=gh_branch.ns, repository=gh_branch.name
+            )
+        except github3.exceptions.NotFoundError as err:
+            msg = (
+                f"App has not been authorized by {gh_branch.ns}, or repo "
+                f"{gh_branch.ns}/{gh_branch.name} does not exist"
+            )
+            logging.error(msg)
+            raise Exception(msg) from err
+
+        gh_app.login_as_app_installation(credentials.app_key, credentials.app_id, install.id)
+        return gh_app
+
+    def _get_github_user_logged_in_app(self) -> github3.GitHub:
+        logging.info("Logging to GitHub as a User")
+        gh_app = github3.GitHub()
+        gh_app.login(token=self.user_token)
+        return gh_app
